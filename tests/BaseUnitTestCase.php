74a075049c3c1ef1205380c7641ead3a61ffbc22
Sam Keen <sam@shopigniter.com>
2011-12-20 08:49:48 -0800

    tests are now using the Core autoload

--- tests/BaseUnitTestCase.php
+++ tests/BaseUnitTestCase.php
@@ -2,16 +2,9 @@
 /**
  * 
  */
+require __DIR__."/../lib/Core.php";
+spl_autoload_register('clear\Core::autoload');
 class BaseUnitTestCase extends PHPUnit_Framework_TestCase {
 		
-	static function autoload($class)
-	{
-		$lib_dir = realpath(__DIR__.'/../lib');
-		$class = str_replace('\\', '/', $class) . '.php';
-		if( ! strstr($class, 'clear/')) {return false;}
-		$class = str_replace('clear/','',$class);
-    	require("$lib_dir/$class");
-	}
+	
 }
-
-spl_autoload_register('BaseUnitTestCase::autoload');
